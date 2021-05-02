b983fd279de416e71164a4a30f5665dbad73b56d
David Solera <david.solera@dxc.com>
2021-05-02 19:07:16 +0200

    Updated Start script for deployment purposes.

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@
 
 
 module.exports = {
+/*  
   ENV: process.env.ENV,
   dbInstance: process.env.dbInstance,
   dbUser: process.env.dbUser,
@@ -9,4 +10,12 @@ module.exports = {
   dbHost: process.env.dbHost,
   dbPort: process.env.dbPort,
   dbDialect: process.env.dbDialect,
+*/  
+  ENV: 'PRO',
+  dbInstance: 'd5r6sbnvtesln9',
+  dbUser: 'ertovrbthcqmhe',
+  dbPassword: 'b17b0c1582882bdf1248198b55b53b952cf4f02f3e0098a1e3ed9de0a516a5cf',
+  dbHost: 'ec2-54-155-87-214.eu-west-1.compute.amazonaws.com',
+  dbPort: 5432,
+  dbDialect: 'postgres'
 };
