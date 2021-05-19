2d130f0b90f843c2aa9e714ea916cd282b3d6f52
David Solera <david.solera@dxc.com>
2021-05-19 18:52:24 +0200

    Updated DB config

--- config/config.js
+++ config/config.js
@@ -12,10 +12,10 @@ module.exports = {
   dbDialect: process.env.dbDialect,
 */  
   ENV: 'PRO',
-  dbInstance: 'd5r6sbnvtesln9',
-  dbUser: 'ertovrbthcqmhe',
-  dbPassword: 'b17b0c1582882bdf1248198b55b53b952cf4f02f3e0098a1e3ed9de0a516a5cf',
-  dbHost: 'ec2-54-155-87-214.eu-west-1.compute.amazonaws.com',
+  dbInstance: 'd2vnaivmpcfj8m',
+  dbUser: 'gqfwvllxdyyqgu',
+  dbPassword: '2779eea6e28dbd14ef54a7dcfb82a158b0729879c2d0d8807fe139b153bb2747',
+  dbHost: 'ec2-34-253-116-145.eu-west-1.compute.amazonaws.com',
   dbPort: 5432,
   dbDialect: 'postgres'
 };
